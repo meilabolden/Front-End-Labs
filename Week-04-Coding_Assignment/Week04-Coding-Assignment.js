f29b58455f70535660b0be1a4d88d7a1e5dd360d
Meila Bolden <meilabolden@gmail.com>
2023-04-27 21:33:09 -0700

    Corrected coding on Step 4 & reassigned step numbers.

--- Week-04-Coding_Assignment/Week04-Coding-Assignment.js
+++ Week-04-Coding_Assignment/Week04-Coding-Assignment.js
@@ -1,6 +1,6 @@
 /* Question 1: Create and array with the following values: 3, 9, 23, 64, 2, 8, 28, 93. */
 
-let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];            /* ages Array */
 
 /*Subtracting the value of the first element (3) in the array from the last element (93) */
 
@@ -8,9 +8,9 @@ console.log('Step 1: 93 - 3 = ' + (ages[ages.length - 1] - ages[0])); /*Should r
 
 /* Adding a new age to the array and repeating the previous step to ensure that the code is dynamic. */
 
-const count = ages.push(35);
-console.log(ages); /* New array should be: [3, 9, 23, 64, 2, 8, 28, 93, 35] */
-console.log('Step 2: 35 - 3 = ' + (ages[ages.length - 1] - ages[0])); /*Should return: 32 (35 - 3 = 32) */
+const count = ages.push(35);                                            /* Adds age 35 to the array. */
+console.log('Step 2: New ages array is: ' + ages);                      /* New array should be: [3, 9, 23, 64, 2, 8, 28, 93, 35] */
+console.log('Step 3: 35 - 3 = ' + (ages[ages.length - 1] - ages[0]));   /*Should return: 32 (35 - 3 = 32) */
 
 /* Use a loop to iterate through the array and calculate the average age. */
 
@@ -23,14 +23,13 @@ function averageAge(ages) {
     }
     return sum;
 }
-console.log('Step 3: The average age is ' + (averageAge([3, 9, 23, 64, 2, 8, 28, 93, 35]) / 9));  /* Average age: 3 + 9 + 23 + 64 + 2 + 8 + 28 + 93 + 35 = 265; 265 / 9 = 29.444444444444444  */
-console.log('Step 3: The average age is ' + (averageAge([i]) / (ages[ages.length - 1])));  /* Average age: 3 + 9 + 23 + 64 + 2 + 8 + 28 + 93 + 35 = 265; 265 / 9 = 29.444444444444444  */
+console.log('Step 4: The average age is ' + ((averageAge(ages)) / (ages.length)));  /* Average age: 3 + 9 + 23 + 64 + 2 + 8 + 28 + 93 + 35 = 265; 265 / 9 = 29.444444444444444  */
 
-/* Array of names: "Sam", "Tommy", "Tim", "Sally", "Buck", "Bob". Get average letters per name. */
+/* Question 2: Array of names: "Sam", "Tommy", "Tim", "Sally", "Buck", "Bob". Get average letters per name. */
 
-let names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+let names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];            /* names Array */
 
-function averageLettersPerName(names) {
+function avgLettersPerName(names) {
 
     for (let i = 0; i < names.length; i++){
         sum += names[i];
@@ -38,4 +37,4 @@ function averageLettersPerName(names) {
 
 }
 
-
+                                
