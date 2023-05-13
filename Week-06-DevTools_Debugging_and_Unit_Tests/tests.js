23e1bdf0830dcba5fe4ce559f7fb33490136c92e
Meila Bolden <meilabolden@gmail.com>
2023-05-12 20:54:00 -0700

    updating week06 labs

--- Week-06-DevTools_Debugging_and_Unit_Tests/tests.js
+++ Week-06-DevTools_Debugging_and_Unit_Tests/tests.js
@@ -62,15 +62,135 @@ describe('Week 6 Lab Tests:', () => {
   })
 
 describe('Question 1a: Sorted array of numbers', () => {
-  it ('#Should sort an array in ascending order', () => {
-    const arrayOfNumbers = [1, 5, 3, 2, 4]
-    arrayOfNumbers.sort((a, b) => a - b)
+    it ('#Should sort an array in ascending order', () => {
+      const arrayOfNumbers = [1, 5, 3, 2, 4]
+      arrayOfNumbers.sort((a, b) => a - b)
 
-    expect(arrayOfNumbers).to.deep.equal([1, 2, 3, 4, 5])
+      expect(arrayOfNumbers).to.deep.equal([1, 2, 3, 4, 5])
+    })
   })
-})
+
+describe('Question 1b: Mange my wallet', () => {
+    class Wallet {
+      constructor(startingMoney) {
+        this.money = startingMoney
+      }
+    
+      addMoney(amount) {
+        this.money += amount
+      }
+    
+      removeMoney(amount) {
+        this.money -= amount
+      }
+    }
+
+      it('#Should be able to add money to my wallet', () => {
+        let myWallet = new Wallet(10)
+        myWallet.addMoney(5)
+
+        expect(myWallet.money).to.equal(15)
+      })
+
+        it('#Should be able to remove money from my wallet', () => {
+          let myWallet = new Wallet(10)
+          myWallet.removeMoney(5)
+
+          expect(myWallet.money).to.equal(5)
+        })
+
+
+describe('Question 1c: Day of the Week', () => {
+    const dayOfTheWeek = (num) => {
+      switch (num) {
+        case 1:
+          return 'Monday'
+            break;
+        case 2:
+          return 'Tuesday'
+            break;
+        case 3:
+          return 'Wednesday'
+            break;
+        case 4:
+          return 'Thursday'
+            break;
+        case 5:
+          return 'Friday'
+            break;
+        case 6:
+          return 'Saturday'
+            break;
+        case 7:
+          return 'Sunday'
+            break;
+        default:
+          console.log('Err. Something went wrong.')
+      }
+    }
+    
+      it('#Should be able to display the correct day of the week', () => {
+
+        expect(dayOfTheWeek(4)).to.equal('Thursday')
+      })
+  })      
+
+  describe('Question 1d: Only wizards should pass', () => {
+    const movieCharacters = [
+      {
+        name: 'Howl',
+        isAWizard: true,
+        quote: `You're wearing that hat? After all the magic I used to make your dress pretty?`,
+      },
+      {
+        name: 'Kalcifer',
+        isAWizard: false,
+        quote: `I don't cook! I'm a scary and powerful fire demon!`,
+      },
+      {
+        name: 'Gandalf',
+        isAWizard: true,
+        quote: `You shall not pass!`,
+      },
+      {
+        name: 'Luke Skywalker',
+        isAWizard: false,
+        quote: `May the Force be with you.`,
+      },
+    ]
+    
+    function onlyWizards(arrayOfCharacters) {
+      return arrayOfCharacters.filter((character) => character.isAWizard == true)
+    }
+    
+    it('#Should return an array of objects. object.isAWizard should be true', () => {
+      
+      // expect(onlyWizards(movieCharacters)).to.deep.equal([{
+      //   name: 'Howl',
+      //   isAWizard: true,
+      //   quote: `You're wearing that hat? After all the magic I used to make your dress pretty?`,
+      // },     {
+      //   name: 'Gandalf',
+      //   isAWizard: true,
+      //   quote: `You shall not pass!`,
+      // },])
+
+    })
+    
+    for (let character of onlyWizards(movieCharacters)) {
+      it('#Character should be a wizard', () => {
+        expect(character.isAWizard).to.equal(true)
+      })
+    }
+
+  })
+  })
 
   /*--------------------------NEW TESTS BELOW-------------------------------*/
 })
+
+
+
+
 
 
