273f6e719769af85d7ddbabc53b5b162d584a453
Meila Bolden <meilabolden@gmail.com>
2023-05-15 14:35:19 -0700

    Added classes to week6 coding assignment

---
+++ Week-06-Coding-Assignment/week06-CoAssign.js
@@ -0,0 +1,120 @@
+// Week 06 Card Game coding assignment
+//
+// Coding Steps:
+//
+// For the final project you will be creating an automated version of the classic card game WAR! 
+// There are many versions of the game WAR. In this version there are only 2 players.
+// You do not need to do anything special when there is a tie in a round.
+// Think about how you would build this project and write your plan down. 
+// Consider classes such as: Card, Deck, Player, as well as what properties and methods they may include. 
+// You do not need to accept any user input, when you run your code, the entire game 
+//  should play out instantly without any user input inside of your browser’s console.
+//
+// The completed project should, when executed, do the following:
+//
+// Deal 26 Cards to each Player from a Deck of 52 cards.
+// Iterate through the turns where each Player plays a Card.
+// The Player who played the higher card is awarded a point
+// Ties result in zero points for both Players
+// After all cards have been played, display the score and declare the winner.
+// Write a Unit Test using Mocha and Chai for at least one of the functions you write.
+
+
+// Card anatomy: NumValue (2,3,4,5,6,7,8,9,10), Suits (heart, diamond, spade, clubs), Face 
+//     - not needed(Back, Boarder, Finish, Stock)
+// Deck: has 52 cards split evenly between 2 players(26 each), 
+// Dealer: shuffles and deals cards
+
+// Game class represents the card game.
+
+console.log('WAR!');
+
+class Game{
+    constructor(turn, score, value){
+        this.turn = turn;
+        this.score = score;
+        this.value = value;
+    }
+}
+
+// Player class represents each player in the game.
+
+class Player{
+    constructor(player,hand) {
+        this.player = player;
+        this.hand = hand;
+    }
+}        
+
+ // // Card class represents all cards in the deck.
+        
+class Card {
+    constructor(face, suits, value){
+        this.face = face;
+        this.suits = suits;
+        this.value = value;
+    }   
+}
+
+// //Deck class represents all 52 cards.
+        
+class Deck {
+    constructor(){
+        this.deck = this.createDeck()
+    }
+        createDeck(){
+            let newDeck = [];
+            let face = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
+            let suits = ['🧡', '🍀', '💠', '🧩']
+                                 
+            for (let f of face) {
+                for (let s of suits) {
+                    let card = new Card(f,s);
+                    newDeck.push(card);
+                }
+            }
+            return newDeck;
+        }
+}   console.log(this.createDeck);
+    
+
+    
+    
+        
+
+
+let game = new Game()
+    
+/************************************************************/
+
+// Live class practice
+/**
+ * 4 suits
+ * 13 cards / suit 2 - A
+ * 52 unique cards
+ */
+
+// Deck of cards
+// Shuffle and deal actions // iterate
+// scores for each player, # of cards
+// Create players - the could have scores
+
+
+
+
+
+
+
+
+
+
+
+
+// let front = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
+// let suits = ['of hearts', 'of diamonds', 'of clubs', 'of spades']
+
+// for (let f of front) {
+//     for (let s of suits) {
+//         let card = new Card(f,s);
+//     }
+// }
---
+++ Week-06-Coding-Assignment/week06-War-Tests.js
@@ -0,0 +1,14 @@
+// Code Testing with Mocha & Chai for the card game War
+
+const expect = chai.expect
+const assert = chai.assert
+
+// Unit Tests & TDD w/ Mocha & Chai.
+
+describe('Test 1: Deal Cards', () => {
+    it('#Should split the 52 card deck in half and deal 26 cards to each player.') 
+        //insert code to be run below
+
+        expect(Deck / 2)
+});
+
