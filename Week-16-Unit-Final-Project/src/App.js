bcbc567bec4321d4fa88ea419de71efd6ea8da66
Meila Bolden <meilabolden@gmail.com>
2023-08-20 18:12:20 -0700

    adding PDF

--- Week-16-Unit-Final-Project/src/App.js
+++ Week-16-Unit-Final-Project/src/App.js
@@ -1,5 +1,7 @@
 // Coding Steps:
-// Using an online API of your choice (or if no API can be found, using an array for in-memory storage is okay as well), create a React project of your choice. You will be working on this for the next three weeks. 
+// Using an online API of your choice (or if no API can be found, 
+//  using an array for in-memory storage is okay as well), create a 
+//  React project of your choice. You will be working on this for the next three weeks. 
 // Project must meet the following criteria:
 // Use React Router and have at least 3 pages using React Bootstrap or an alternative styling library
 // Contain at least 10 components
